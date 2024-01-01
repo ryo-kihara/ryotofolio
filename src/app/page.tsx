8e32fece0be6d3aaf369517434e9f1319922898a
ryo-kihara <work1008.rk@gmail.com>
2024-01-01 16:36:57 +0900

    Add new dependencies and files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,80 +1,79 @@
+'use client'
+
+import HeatMap, { SVGProps } from '@uiw/react-heat-map'
 import Image from 'next/image'
 
 import styles from './page.module.css'
 
+const value = [
+  { date: '2016/01/11', count: 2 },
+  { date: '2016/01/12', count: 20 },
+  { date: '2016/01/13', count: 10 },
+  ...[...Array(17)].map((_, idx) => ({ date: `2016/02/${idx + 10}`, count: idx, content: '' })),
+  { date: '2016/04/11', count: 2 },
+  { date: '2016/05/01', count: 5 },
+  { date: '2016/05/02', count: 5 },
+  { date: '2016/05/04', count: 11 },
+]
+
+const width = 760
+const weekLabels = ['', 'Mon', '', 'Wed', '', 'Fri', '']
+const startDate = new Date('2016/01/01')
+const rectSize = 12
+const legendRender: SVGProps['legendRender'] = (props) => <rect {...props} key={props.key} rx={2.5} />
+const rectRender: SVGProps['rectRender'] = (props) => <rect {...props} key={props.key} rx={2.5} />
+const xPanelColors = {
+  0: '#EBEDF0',
+  4: '#DBE9F6',
+  8: '#A4CCF4',
+  12: '#4A99E9',
+  32: '#000000',
+}
+const googleTodoPanelColors = {
+  0: '#EBEDF0',
+  4: '#CFDDF8',
+  8: '#93B2EE',
+  12: '#3871E0',
+  32: '#1d3a73',
+}
+
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image src="/vercel.svg" alt="Vercel Logo" className={styles.vercelLogo} width={100} height={24} priority />
-          </a>
-        </div>
-      </div>
+      <Image src="/github.svg" alt="Github Logo" width={40} height={40} />
+      <HeatMap
+        value={value}
+        width={width}
+        weekLabels={weekLabels}
+        startDate={startDate}
+        rectSize={rectSize}
+        legendRender={legendRender}
+        rectRender={rectRender}
+      />
 
-      <div className={styles.center}>
-        <Image className={styles.logo} src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
-      </div>
+      <Image src="/x.png" alt="X Logo" width={40} height={40} />
+      <HeatMap
+        value={value}
+        width={width}
+        weekLabels={weekLabels}
+        startDate={startDate}
+        rectSize={rectSize}
+        legendRender={legendRender}
+        rectRender={rectRender}
+        panelColors={xPanelColors}
+      />
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
-        </a>
-      </div>
+      <Image src="/google-todo.png" alt="Google ToDo Logo" width={40} height={40} />
+      <HeatMap
+        value={value}
+        width={width}
+        weekLabels={weekLabels}
+        startDate={startDate}
+        rectSize={rectSize}
+        legendRender={legendRender}
+        rectRender={rectRender}
+        panelColors={googleTodoPanelColors}
+      />
     </main>
   )
 }
